ab0471e58b4f7882ad732939bbe3ef12cb766cc2
taepark0302 <105846112+taepark0302@users.noreply.github.com>
2022-10-12 23:51:07 +0900

    Update main.tf

--- 2-terraform/09-s3-backend/main.tf
+++ 2-terraform/09-s3-backend/main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "fastcampus-devops-terraform"
+    bucket = "-devops-terraform"
     key    = "s3-backend/terraform.tfstate"
     region = "ap-northeast-2"
   }
